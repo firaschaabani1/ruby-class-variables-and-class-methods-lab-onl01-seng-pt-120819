275b364467344b7147e6c1a6aa1ccf8a66deda04
Firas Chaabani <firaschaabani1@gmail.com>
2020-01-21 02:07:43 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+song.new
+song = {"lucifer", "Jay-Z", "rap" }
+son.class_variable_set.new
+def name (song)
+  @name : name
+end
+def genre (song)
+  @genre : genre
+end
+def artist (song)
+  @artist : artist
+end
